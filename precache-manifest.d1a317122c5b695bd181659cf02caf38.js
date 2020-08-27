29503e40a3668e95b7f7623d8e5bc1a19cb38761
Nicolás Maximiliano <nico.caruso1992@outlook.com>
2020-08-27 13:47:23 -0300

    Updates

---
+++ precache-manifest.d1a317122c5b695bd181659cf02caf38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbe87edfa999bc6914460148356f2bb",
+    "url": "/RickYMorty/index.html"
+  },
+  {
+    "revision": "1bacee41635126dceedb",
+    "url": "/RickYMorty/static/js/2.f7caf926.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/RickYMorty/static/js/2.f7caf926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80bd2f5dd2c3d5f271d8",
+    "url": "/RickYMorty/static/js/main.a4d8fd56.chunk.js"
+  },
+  {
+    "revision": "761d974ea53050cd6095",
+    "url": "/RickYMorty/static/js/runtime-main.e7dde3c8.js"
+  },
+  {
+    "revision": "dcae35426033ba7c6f7b9872f4df0751",
+    "url": "/RickYMorty/static/media/background2.dcae3542.jpg"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/RickYMorty/static/media/logo.75509f2e.png"
+  }
+]);
